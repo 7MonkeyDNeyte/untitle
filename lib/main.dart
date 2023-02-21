1bf5ad18277132eabab728c1cf53ed2a62869483
7MonkeyDNeyte <danmalewal@gmail.com>
2023-02-21 15:25:44 +0100

    sdfb

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Medico App',
+      title: 'HyHeath',
       theme: ThemeData(
       
         primarySwatch: Colors.blue,
